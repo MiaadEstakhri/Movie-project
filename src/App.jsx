7b8d56e592dff65b1af6cd9a99018bec2c72e6e5
MiaadEstakhri <miaad8104@gmail.com>
2023-11-12 20:37:55 +0330

    import  navBar

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,16 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Navbar from "./components/navbar";
 
 function App() {
-  return <div className="bg-black">hello</div>;
+  return (
+    <main className="w-100 h-screen bg-slate-800 flex justify-center">
+      <div className="container mx-2 sm:mx-auto">
+        <Navbar />
+      </div>
+    </main>
+  );
 }
 
 export default App;
