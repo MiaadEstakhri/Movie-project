a058f2469f6399b7643cb4c82af9001237b65ed7
MiaadEstakhri <miaad8104@gmail.com>
2023-11-12 21:23:59 +0330

    add heart

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,15 +2,20 @@ const Navbar = () => {
   return (
     <nav className="flex justify-between items-center bg-slate-700 text-white rounded-xl py-3 px-4 mt-8 ">
       <div className="font-bold ">LOGO</div>
-      <div className="w-24 sm:w-36 md:w-44 xl:w-56 bg-slate-500 py-1.5 px-3 rounded-md ">
+      <div className="w-24 sm:w-36 md:w-44 xl:w-56 bg-slate-500 py-1.5 px-3 rounded-lg ">
         <input
           type="text"
           placeholder="search ..."
           className="bg-transparent outline-none"
         />
       </div>
-      <div className="">Found X Results</div>
-      <div className="text-3xl">♡</div>
+      <div className="text-gray-400">Found X Results</div>
+      <div className="relative">
+        <span className="h-5 w-5 flex justify-center items-center bg-red-600 text-white rounded-full text-xs absolute left-6 top-0">
+          3
+        </span>
+        <span className="text-4xl text-red-600">♡</span>
+      </div>
     </nav>
   );
 };
