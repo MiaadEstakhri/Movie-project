d9cefc33620d5e29adeb031a69afd9cc715ae1af
MiaadEstakhri <miaad8104@gmail.com>
2023-11-12 23:37:28 +0330

    refactor style

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ import CharacterList from "./components/characterList";
 
 function App() {
   return (
-    <main className="w-100 h-screen bg-slate-800 flex justify-center">
-      <div className="container mx-2 sm:mx-auto">
+    <main className="w-100 h-screen flex justify-center">
+      <div className="container mx-3">
         <Navbar />
         <section className="grid grid-flow-col  grid-cols-7">
-          <section className=" col-span-2">
+          <section className=" col-span-3 lg:col-span-2">
             <CharacterList />
           </section>
-          <section className=" bg-blue-700 col-span-5">3</section>
+          <section className=" bg-blue-700 col-span-4 lg:col-span-5">3</section>
         </section>
       </div>
     </main>
