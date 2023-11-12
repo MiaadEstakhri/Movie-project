30cb96ccdcea0908771e4b463986a79ea9a7ff42
MiaadEstakhri <miaad8104@gmail.com>
2023-11-12 22:35:00 +0330

    import characterList

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,19 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "./components/navbar";
+import CharacterList from "./components/characterList";
 
 function App() {
   return (
     <main className="w-100 h-screen bg-slate-800 flex justify-center">
       <div className="container mx-2 sm:mx-auto">
         <Navbar />
+        <section className="grid grid-flow-col  grid-cols-7">
+          <section className=" col-span-2">
+            <CharacterList />
+          </section>
+          <section className=" bg-blue-700 col-span-5">3</section>
+        </section>
       </div>
     </main>
   );
