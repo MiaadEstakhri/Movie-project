de2ead5906a73564071ee201243db302600fd715
MiaadEstakhri <miaad8104@gmail.com>
2023-11-14 22:57:37 +0330

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ import CharacterDetail from "./components/characterDetail";
 
 function App() {
   return (
-    <main className="w-100 h-screen flex justify-center">
+    <main className="w-100 h-screen flex justify-center overflow-hidden">
       <div className="container mx-3">
         <Navbar />
         <section className="grid grid-flow-row sm:grid-flow-col  sm:grid-cols-8">
-          <div className=" sm:col-span-4 ">
+          <div className=" sm:col-span-4  2xl:col-span-3 ">
             <CharacterList />
           </div>
-          <div className=" sm:col-span-4 ">
+          <div className=" sm:col-span-4 2xl:col-span-5">
             <CharacterDetail />
           </div>
         </section>
--- src/components/characterDetail.jsx
+++ src/components/characterDetail.jsx
@@ -2,12 +2,12 @@ import { character } from "../data/data";
 
 const CharacterDetail = () => {
   return (
-    <div className=" grid grid-flow-col grid-cols-5 sm:h-[16.5rem] bg-slate-800 my-2 sm:my-6 ms-2 lg:p-4 rounded-2xl">
-      <div className="col-span-2 ">
+    <div className=" grid grid-flow-col grid-cols-5  bg-slate-800 my-2 sm:my-6 ms-2 rounded-2xl ">
+      <div className="col-span-2  ">
         <img
           src={character.image}
           alt={character.name}
-          className="w-full  rounded-s-2xl"
+          className="w-full h-auto xl:max-h-64  rounded-s-2xl"
         />
       </div>
     </div>
