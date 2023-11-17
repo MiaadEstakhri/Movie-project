77ca53602f9d28c541075c628bbab68e4957655c
MiaadEstakhri <miaad8104@gmail.com>
2023-11-17 22:47:45 +0330

    Complete the design

--- src/components/characterDetail.jsx
+++ src/components/characterDetail.jsx
@@ -2,13 +2,42 @@ import { character } from "../data/data";
 
 const CharacterDetail = () => {
   return (
-    <div className=" grid grid-flow-col grid-cols-5  bg-slate-800 my-2 sm:my-6 ms-2 rounded-2xl ">
+    <div className=" grid grid-flow-col grid-cols-5  bg-slate-800 my-2 sm:my-4 sm:ms-4  rounded-2xl ">
       <div className="col-span-2  ">
         <img
           src={character.image}
           alt={character.name}
-          className="w-full h-auto xl:max-h-64  rounded-s-2xl"
+          className="w-full h-full xl:max-h-64  rounded-s-2xl"
         />
+      </div>
+      <div className="col-span-3 flex flex-col justify-around ps-3  py-3 lg:py-0 text-white/90">
+        <div>
+          <h3 className=" whitespace-nowrap  font-bold ">
+            <span className="text-lg">
+              {character.gender === "Male" ? "👱🏻‍♂️" : "👩🏻‍🦰"}
+            </span>
+            <span className="text-base lg:text-xl ps-1">{character.name}</span>
+          </h3>
+          <div className="text-xs lg:text-sm font-light">
+            <span
+              className={`inline-block w-3 h-3  rounded-full ${
+                character.status === "Dead" ? "bg-red-500" : "bg-green-500"
+              }`}></span>
+            <span className=""> {character.status} </span>
+            <span className=""> - {character.species}</span>
+          </div>
+        </div>
+        <div className="text-xs lg:text-sm mt-3">
+          <p className="text-gray-500/40 whitespace-nowrap text-sm ">
+            Last know location:
+          </p>
+          <p className="whitespace-nowrap mt-1 font-semibold">
+            {character.location.name}
+          </p>
+          <button className="bg-slate-500/95 py-2 px-3 md:py-3 sm:px-4 rounded-xl mt-3 font-extrabold ">
+            Add to Favorite
+          </button>
+        </div>
       </div>
     </div>
   );
