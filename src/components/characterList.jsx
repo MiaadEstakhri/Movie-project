4f393215b85ec2dfdc916db0e0709bdffc3d38d7
MiaadEstakhri <miaad8104@gmail.com>
2023-11-17 22:48:20 +0330

    refactor the design

--- src/components/characterList.jsx
+++ src/components/characterList.jsx
@@ -6,7 +6,7 @@ const CharacterList = () => {
     <div className="">
       {allCharacters.map((item) => {
         return (
-          <div className="grid grid-flow-col grid-cols-5  bg-slate-800 my-6 me-2 p-2 sm:p-3 lg:p-4 rounded-2xl">
+          <div className="grid grid-flow-col grid-cols-5  bg-slate-800 my-4  p-2 sm:p-3 lg:p-4 rounded-2xl">
             <div className="col-span-1 sm:col-span-2">
               <img
                 src={item.image}
@@ -14,7 +14,7 @@ const CharacterList = () => {
                 className=" w-full lg:w-[90%] sm:h-24 lg:h-32 xl:h-36  rounded-xl sm:rounded-2xl "
               />
             </div>
-            <div className="col-span-3 flex flex-col justify-around  ps-3 lg:ps-0 text-white">
+            <div className="col-span-3 flex flex-col justify-around  ps-3 lg:ps-0 text-white/90">
               <h3 className=" whitespace-nowrap  font-bold ">
                 <span className="text-lg">
                   {item.gender === "Male" ? "👱🏻‍♂️" : "👩🏻‍🦰"}
@@ -23,7 +23,7 @@ const CharacterList = () => {
               </h3>
               <div className="text-xs lg:text-sm ">
                 <span
-                  className={`inline-block w-3 h-3  rounded-full ${
+                  className={`inline-block w-3 h-3  rounded-full font-light ${
                     item.status === "Dead" ? "bg-red-500" : "bg-green-500"
                   }`}></span>
                 <span className=""> {item.status} </span>
