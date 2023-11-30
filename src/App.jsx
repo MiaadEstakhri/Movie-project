eb9fce188dd0ce2cd97f95b4fe3e7d826874d5ab
MiaadEstakhri <miaad8104@gmail.com>
2023-11-30 19:19:13 +0330

    add state  and Passing it to Navbar and characterList

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import "./App.css";
+import { useState } from "react";
 import Navbar from "./components/navbar";
 import CharacterList from "./components/characterList";
 import CharacterDetail from "./components/characterDetail";
 import CharacterEpisodes from "./components/characterEpisodes";
+import { allCharacters } from "./data/data";
+import "./App.css";
 
 function App() {
+  const [character, setCharacter] = useState(allCharacters);
   return (
     <main className="w-100 h-screen flex justify-center overflow-hidden">
       <div className="container mx-3">
-        <Navbar />
+        <Navbar numOfResult={character.length} />
         <section className="grid grid-flow-row sm:grid-flow-col  sm:grid-cols-8">
           <div className=" sm:col-span-4  2xl:col-span-3 ">
-            <CharacterList />
+            <CharacterList characters={character} />
           </div>
           <div className=" sm:col-span-4 2xl:col-span-5 mx-[.125rem] sm:mx-0 my-2 sm:my-4 sm:ms-4 ">
             <CharacterDetail />
