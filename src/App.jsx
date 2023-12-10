986a5da0165f05ec90379626b8c68b3052826296
MiaadEstakhri <miaad8104@gmail.com>
2023-12-10 21:49:01 +0330

    add Api in useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./components/navbar";
 import CharacterList from "./components/characterList";
 import CharacterDetail from "./components/characterDetail";
@@ -7,16 +7,25 @@ import { allCharacters } from "./data/data";
 import "./App.css";
 
 function App() {
-  const [character, setCharacter] = useState(allCharacters);
+  const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+    async function fetchApi() {
+      const res = await fetch("https://rickandmortyapi.com/api/character");
+      const data = await res.json();
+      setCharacters(data.results);
+    }
+    fetchApi();
+  }, []);
+
   return (
-    <main className="w-100 h-screen flex justify-center overflow-hidden">
+    <main className="w-100  flex justify-center overflow-hidden">
       <div className="container mx-3">
-        <Navbar numOfResult={character.length} />
-        <section className="grid grid-flow-row sm:grid-flow-col  sm:grid-cols-8">
-          <div className=" sm:col-span-4  2xl:col-span-3 ">
-            <CharacterList characters={character} />
+        <Navbar numOfResult={characters.length} />
+        <section className="grid grid-flow-row sm:grid-flow-col  sm:grid-cols-8 mt-6">
+          <div className=" sm:col-span-4  2xl:col-span-3 mt-4">
+            <CharacterList characters={characters} />
           </div>
-          <div className=" sm:col-span-4 2xl:col-span-5 mx-[.125rem] sm:mx-0 my-2 sm:my-4 sm:ms-4 ">
+          <div className=" sm:col-span-4 2xl:col-span-5 mx-[.125rem] sm:mx-0  sm:my-4 sm:ms-4 ">
             <CharacterDetail />
             <CharacterEpisodes />
           </div>
