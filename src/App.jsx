1e297fdfdc47b3a4d53e55e6f8a015dce4e3fa91
MiaadEstakhri <miaad8104@gmail.com>
2023-12-14 23:17:23 +0330

    Giving status to search from Characters

--- src/App.jsx
+++ src/App.jsx
@@ -11,29 +11,35 @@ import "./App.css";
 function App() {
   const [characters, setCharacters] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     async function fetchApi() {
       try {
         setIsLoading(true);
         const { data } = await axios.get(
-          "https://rickandmortyapi.com/api/character"
+          `https://rickandmortyapi.com/api/character/?name=${query}`
         );
         setCharacters(data.results);
       } catch (error) {
+        setCharacters([]);
         toast.error(error.response.data.error);
       } finally {
         setIsLoading(false);
       }
     }
     fetchApi();
-  }, []);
+  }, [query]);
 
   return (
     <main className="w-100 h-screen  flex justify-center ">
       <Toaster />
       <div className="h-100 container mx-4 xl:mx-0">
-        <Navbar numOfResult={characters.length} />
+        <Navbar
+          numOfResult={characters.length}
+          query={query}
+          setQuery={setQuery}
+        />
         <section className="grid grid-flow-row sm:grid-flow-col  sm:grid-cols-8 mt-6">
           <div className=" sm:col-span-4  2xl:col-span-3 mb-5 sm:mb-0  sm:mt-4 text-center">
             {isLoading ? (
