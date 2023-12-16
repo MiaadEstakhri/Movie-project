021b07df1412117c6244088bc22f185453ebf164
MiaadEstakhri <miaad8104@gmail.com>
2023-12-16 23:38:49 +0330

    add animation loader

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -1,6 +1,8 @@
 function Loading() {
   return (
-    <p className="text-yellow-50 font-bold text-2xl  my-8">Loading Data...</p>
+    <div className="flex justify-center items-center text-white/90 font-bold text-2xl  my-8 ">
+      <span className="me-2">Loading Data</span> <div className="loader"></div>
+    </div>
   );
 }
 
