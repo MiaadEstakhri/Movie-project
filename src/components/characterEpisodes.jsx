625bdfcc3c4367bfddaf2d96cb2f826d7116e86b
MiaadEstakhri <miaad8104@gmail.com>
2023-12-25 22:16:08 +0330

    added sort for episodes

--- src/components/characterEpisodes.jsx
+++ src/components/characterEpisodes.jsx
@@ -1,21 +1,44 @@
+import { useState } from "react";
 import ArrowUpCircleIcon from "../assets/icons/arrowUpCircleIcon";
+import ArrowDownCircleIcon from "../assets/icons/arrowDownCircle";
 
 function CharacterEpisodes({ episodes }) {
+  const [sortBy, setSortBy] = useState(true);
+
+  let sortedEpisode;
+
+  if (sortBy) {
+    sortedEpisode = [...episodes].sort(
+      (a, b) => new Date(a.created) - new Date(b.created)
+    );
+  } else {
+    sortedEpisode = [...episodes].sort(
+      (a, b) => new Date(b.created) - new Date(a.created)
+    );
+  }
+
   return (
     <div className="w-100  bg-slate-800 rounded-xl mt-5 p-3 ">
       <div className="w-100 flex justify-between whitespace-nowrap mb-1">
         <h2 className="text-slate-500 font-semibold sm:text-lg tracking-widest">
           List of Episodes:
         </h2>
-        <button className="">
-          <ArrowUpCircleIcon
-            color="#f9fafb"
-            className="h-6 w-6 sm:h-7 sm:w-7"
-          />
+        <button className="" onClick={() => setSortBy((is) => !is)}>
+          {sortBy ? (
+            <ArrowUpCircleIcon
+              color="#f9fafb"
+              className="h-6 w-6 sm:h-7 sm:w-7"
+            />
+          ) : (
+            <ArrowDownCircleIcon
+              color="#f9fafb"
+              className="h-6 w-6 sm:h-7 sm:w-7"
+            />
+          )}
         </button>
       </div>
       <ul className="text-white/80 whitespace-nowrap">
-        {episodes.map((item, index) => {
+        {sortedEpisode.map((item, index) => {
           return (
             <li
               className="flex justify-between mt-3  text-[.7rem] md:text-xs "
