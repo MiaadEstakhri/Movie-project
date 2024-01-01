3e3a5059a9f7291c118a9c7b5f556cfc791d8f7c
MiaadEstakhri <miaad8104@gmail.com>
2024-01-01 22:06:20 +0330

    added favorites character

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,15 @@
 import HeartIcon from "../assets/icons/heartIcon";
 import logo from "../assets/images/images_prev_ui(2).png";
 
-function Navbar({ numOfResult, query, setQuery, numOfFavorites }) {
+function Navbar({
+  numOfResult,
+  query,
+  setQuery,
+  favorites,
+  onSelectFavorites,
+}) {
   return (
-    <nav className="flex justify-between items-center bg-slate-700 text-white rounded-xl py-2 px-4 mt-8 ">
+    <nav className="flex justify-between items-center bg-slate-700 text-white rounded-lg py-2 px-4 mt-8 ">
       <a href="#" className="">
         <img src={logo} alt="Logo" className="w-24 sm:w-28" />
       </a>
@@ -24,10 +30,11 @@ function Navbar({ numOfResult, query, setQuery, numOfFavorites }) {
       </div>
       <div className="relative me-5">
         <span className="h-4 w-4 sm:h-5 sm:w-5 flex justify-center items-center bg-red-600 text-white rounded-full text-xs absolute left-4 sm:left-6 bottom-[1.1rem] sm:bottom-[1.3rem]">
-          {numOfFavorites}
+          {favorites.length}
         </span>
-
-        <HeartIcon color="#dc2626" className="w-7 h-7 sm:w-9 sm:h-9" />
+        <button onClick={onSelectFavorites}>
+          <HeartIcon color="#dc2626" className="w-7 h-7 sm:w-9 sm:h-9" />
+        </button>
       </div>
     </nav>
   );
