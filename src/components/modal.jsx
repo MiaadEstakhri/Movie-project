571f4ef3deb9f115ef8d5fd010e71826bb0ec9de
MiaadEstakhri <miaad8104@gmail.com>
2024-01-02 23:00:21 +0330

    refactor design modal

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -3,7 +3,7 @@ import XCircleIcon from "../assets/icons/xCircleIcon";
 function Modal({ header, children, open, onOpen }) {
   if (!open) return null;
   return (
-    <div className="w-full h-screen absolute flex  justify-center items-center overflow-hidden ">
+    <div className="w-full h-screen absolute flex  justify-center items-center overflow-hidden px-3">
       <div className="h-full  w-full z-10 bg-slate-400/60 absolute"></div>
       <div className="flex flex-col w-[500px] min-h-[300px] max-h-[400px] bg-slate-800 z-20 rounded-lg px-3 ">
         <div className="flex items-center justify-between text-slate-50  text-xl font-bold border-b-2 border-neutral-300/20 py-5">
